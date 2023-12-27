05740a64b8cd11e6b6b0e095f7360aa9db9f5038
PRAPTI220098 <140289767+PRAPTI220098@users.noreply.github.com>
2023-12-27 12:18:38 +0530

    main.py

--- main.py
+++ main.py
@@ -36,4 +36,20 @@ def chatbot(uid, text):
     res = url['candidates'][0]['content']['parts'][0]['text']
     return res
 
-bot.infinity_polling()
+
+server = flask.Flask(__name__)
+
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    link = 'https://'+str(flask.request.host)
+    bot.set_webhook(url=f"{link}/bot")
+    return "Success!", 200
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=81)
