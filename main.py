cce436b5c2bb6c649e86f13291c15966cf1d9f04
PRAPTI220098 <140289767+PRAPTI220098@users.noreply.github.com>
2024-02-09 23:18:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import requests
 from datetime import datetime, timedelta
 import flask
 from user_agent import generate_user_agent as rrr
-ttt = os.environ.get('BOT')
-bot = telebot.TeleBot('ttt')
+bot = telebot.TeleBot('6643702223:AAF6u8O7KoJ_Or9cVpOGmDD0RnZ5z_63H-k')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
