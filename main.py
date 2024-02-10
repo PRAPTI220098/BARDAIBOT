1bb4aad0128864e95a6dddb002e5c8c5ce5f5ce5
PRAPTI220098 <140289767+PRAPTI220098@users.noreply.github.com>
2024-02-10 09:38:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import requests
 from datetime import datetime, timedelta
 import flask
 from user_agent import generate_user_agent as rrr
-bot = telebot.TeleBot('6643702223:AAGf-EKPO5Z4LzyAplYgEaE2yb86X3J0Qac')
-
+ts=os.environ.get('TOKEN')
+bot = telebot.TeleBot(ts)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    usr = message.from_user.first_name
-    text = f"Welcome, [{usr}](tg://settings)! 🤖\n\n_🎉🤖 Welcome to BARD AI Bot! 🌟🚀_\n\n_Hey there, fellow humanoids! 🤗 I'm the party starter, the chat sensation, the one and only BARD AI Bot! 🎩💬_\n\n_I'm equipped with wit, humor, and an arsenal of emojis! 🤓✨ Let's chat and sprinkle some sparkles of fun all around! 🌈✨_\n\n_Whether it's a joke, a story, or just a friendly chat, I'm here for it! 📚🗨️\nSo, how's your day going? Tell me everything! 🌞🌸_"
-    keyboard = types.InlineKeyboardMarkup(row_width=1)
-    developer_button = types.InlineKeyboardButton(text='💸 SANCHIT 💸', url='https://t.me/X668F')
-    ista = types.InlineKeyboardButton(text='🔮 Follow Me On Instagram 🔮', url='https://www.instagram.com/sanch1t')
-    keyboard.add(developer_button,ista)
-    bot.reply_to(message, text, parse_mode='markdown', reply_markup=keyboard)
+    	usr = message.from_user.first_name
+    	text = f"Welcome, [{usr}](tg://settings)! 🤖\n\n_🎉🤖 Welcome to BARD AI Bot! 🌟🚀_\n\n_Hey there, fellow humanoids! 🤗 I'm the party starter, the chat sensation, the one and only BARD AI Bot! 🎩💬_\n\n_I'm equipped with wit, humor, and an arsenal of emojis! 🤓✨ Let's chat and sprinkle some sparkles of fun all around! 🌈✨_\n\n_Whether it's a joke, a story, or just a friendly chat, I'm here for it! 📚🗨️\nSo, how's your day going? Tell me everything! 🌞🌸_"
+    	keyboard = types.InlineKeyboardMarkup(row_width=1)
+    	dev = types.InlineKeyboardButton(text='💸 SANCHIT 💸', url='https://t.me/X668F')
+    	ista = types.InlineKeyboardButton(text='🔮 Follow Me On Instagram 🔮', url='https://www.instagram.com/sanch1t')
+    	keyboard.add(dev, ista)
+    	de=requests.get(f"https://translate-api-mu.vercel.app/translate?from=auto&to={message.from_user.language_code}&text={text}").json()['translation']
+    	bot.reply_to(message, de, parse_mode='markdown', reply_markup=keyboard)
 
 @bot.message_handler(func=lambda message: True)
 def reply_to_user(message):
@@ -31,12 +32,13 @@ def chatbot(message, uid, text):
     'Host': 'api.safone.dev',
     'user-agent': str(rrr())
   }
-  tr = requests.get(f"https://translate-api-mu.vercel.app/translate?from=auto&to=en&text={text}").json()['translation']
+  tr = requests.get(f"https://translate-api-mu.vercel.app/translate?from=auto&to={message.from_user.language_code}&text={text}").json()['translation']
   url = requests.get(f"https://api.safone.dev/bard?message={tr}", headers=head).json()
   try:
   	res = url['candidates'][0]['content']['parts'][0]['text']
   	return res
   except:
-  	bot.send_message(message.chat.id,f"_Regrettably, your choice of words includes prohibited language. Kindly opt for more suitable expressions. 🚫🗣️ Let's keep the conversation positive!_ 🌟👍",parse_mode='Markdown')	
+  	de=requests.get(f"https://translate-api-mu.vercel.app/translate?from=auto&to={message.from_user.language_code}&text=_Regrettably, your choice of words includes prohibited language. Kindly opt for more suitable expressions. 🚫🗣️ Let's keep the conversation positive!_ 🌟👍").json()['translation']
+  	bot.send_message(message.chat.id, de,parse_mode='Markdown')	
 
 bot.infinity_polling()
